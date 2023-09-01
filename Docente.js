4d82ef02915e7a233daf388da7c703b8efba8c0e
matheusrodacki <44122620+matheusrodacki@users.noreply.github.com>
2023-09-01 17:54:01 -0300

    Prettier Formated

--- Docente.js
+++ Docente.js
@@ -1,18 +1,28 @@
 import User from "./User.js";
 
 export default class Docente extends User {
-  #codMateria
-  constructor(nome, email, nascimento,codMateria, role = 'docente', ativo = true) {
-    super(nome, email, nascimento, role, ativo)
-    this.#codMateria = codMateria
-  }
-  
-  aprovaEstudante(estudante, curso) {
-    return `estudante ${estudante} passou no curso ${curso}.`
+  #codMateria;
+  constructor(
+    nome,
+    email,
+    nascimento,
+    codMateria,
+    role = "docente",
+    ativo = true
+  ) {
+    super(nome, email, nascimento, role, ativo);
+    this.#codMateria = codMateria;
   }
 
-  get codMateria(){
-    return this.#codMateria
+  aprovaEstudante(estudante, curso) {
+    return `estudante ${estudante} passou no curso ${curso}.`;
+  }
+
+  get codMateria() {
+    return this.#codMateria;
+  }
+
+  exibirInfos() {
+    return `${this.nome}, ${this.email}, ${this.nascimento}, ${this.ativo}, ${this.codMateria} `;
   }
 }
-
